b5b90d8c140a7a516786924ddb5861d107dd57a9
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-02-13 21:06:47 +0100

    docs: Configuration project

---
+++ src/main/java/edu/poniperro/ricksy/RicksyBusiness.java
@@ -0,0 +1,13 @@
+package edu.poniperro.ricksy;
+
+/**
+ * Hello world!
+ *
+ */
+public class RicksyBusiness 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
