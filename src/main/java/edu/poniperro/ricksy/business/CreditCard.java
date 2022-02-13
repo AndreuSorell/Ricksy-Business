4d0b311811d403e3481ec27d09d7c3fadd64adb1
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-02-13 21:37:42 +0100

    feat(main): Primera historia de usuario
    Crear la clase CreditCard

---
+++ src/main/java/edu/poniperro/ricksy/business/CreditCard.java
@@ -0,0 +1,38 @@
+package edu.poniperro.ricksy.business;
+
+import javax.swing.text.html.HTMLDocument.RunElement;
+
+public class CreditCard {
+    private final String owner;
+    private final String number;
+    private double credit = 3000;
+    private final String SYMBOL = "EZI";
+
+    CreditCard(String owner, String number) {
+        this.owner = owner;
+        this.number = number;
+    }
+
+    boolean pay(double credito) {
+    //de momento true
+        return true;
+    }
+
+    String number() {
+        return this.number;
+    }
+    String cardOwner() {
+        return this.owner;
+    }
+
+    double credit() {
+        return this.credit;
+    }
+
+    @Override
+    public String toString() {
+        return  number();
+    }
+
+    
+}
