a5060e10124cd1d1e32a26cf02ce8f277538c2a3
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-02-14 02:07:07 +0100

    test(test): tercera historia de usuario
    Comprobar que se le ha asignado una nave y que se le ha cobrado

--- src/test/java/edu/poniperro/ricksy/business/CreditCardTest.java
+++ src/test/java/edu/poniperro/ricksy/business/CreditCardTest.java
@@ -4,22 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
+public class CreditCardTest {
     CreditCard abradolph = new CreditCard("Abradolph Lincler", "4916119711304546");
     @Test
-    public void creditCard() {
+    public void creditCardTest() {
         assertEquals( "4916119711304546",  abradolph.toString());
-    }
-
-    UfosPark ufo = new UfosPark();
-
-    @Test
-    public void addOvni() {
-        ufo.add("dox");
-        assertEquals( "flota={dox=null}", ufo.toString());
     }
 }
---
+++ src/test/java/edu/poniperro/ricksy/business/UfosParkTest.java
@@ -0,0 +1,35 @@
+package edu.poniperro.ricksy.business;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UfosParkTest {
+    UfosPark ufos = null;
+    String[] ovnis = { "unx", "dox", "trex" };
+
+    @Before
+    public void setupUfosPark() {
+        ufos = new UfosPark();
+        assertNotNull("Parque de UFOS creados", ufos);
+        for (String ovni : ovnis) {
+			ufos.add(ovni);
+        }
+    }
+    
+    @Test
+    public void addOvniTest() {
+        assertEquals( "flota={trex=null, dox=null, unx=null}", ufos.toString());
+    }
+    
+    //comprobamos que se ha reducido el credito de la tarjeta y se le ha assignado una nave
+    @Test
+    public void dispatchTest() {
+        CreditCard abradolph = new CreditCard("Abradolf Lincler", "4916119711304546");
+        ufos.dispatch(abradolph);
+        assertEquals("trex", ufos.getUfoOf(abradolph.number()));
+        assertEquals(2500.0, abradolph.credit(), 0);
+    }
+}
